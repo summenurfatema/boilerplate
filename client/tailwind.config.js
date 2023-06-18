02f7ed71a480aac1c2fda3177c48dd220b598faa
summenurfatema <shadrinmoni15@gmail.com>
2023-06-18 11:18:11 +0600

    edited

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      montserrat: ["Montserrat"],
+      swashed: ["Sansita Swashed", "cursive"],
+      spectral: ["Spectral", "serif"],
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1366px',
+      '3xl': '1440px',
+      '4xl': '1536px',
+      '5xl': '1600px',
+      '6xl': '1920px',
+    },
     extend: {},
   },
   plugins: [],
